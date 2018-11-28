bf49a945b346b624d904ebae19a0db10cf80c4bd
DanielAlexanderBeltran <danbelven@gmail.com>
2018-11-28 11:00:30 -0500

    Examen 1 Bimestre

---
+++ 2018-b-prueba-master/2018-b-prueba-master/01-arreglos/Respuestas.js
@@ -0,0 +1,64 @@
+// Realice los siguientes ejercicios//
+//  1) Busque los tipos de "gender" en el arreglo `people.json`//
+var arreglogender = [];
+arreglogender =[
+    1, "gender: male",
+    2, "gender: female",
+    3, "gender: n/a",
+];
+console.log(arreglogender);
+//2) Busque los tipos de "eye_color" en el arreglo `people.json`//
+var arreglotipos = [];
+arreglotipos =[
+    1, "eye_color: blue",
+    2, "eye_color: yellow",
+    3, "eye_color: red",
+    4, "eye_color: brown",
+    5,"eye_color: blue-gray",
+    6, "eye_color: black",
+    7, "eye_color: orange",
+    8, "eye_color: hazel",
+    9, "eye_color: pink"
+];
+console.log(arreglotipos);
+//3) Busque los tipos de "skin_color" en el arreglo `people.json`
+var arreglotiposkin = [];
+arreglotiposkin =[
+    1, "skin_color: fair",
+    2, "skin_color: gold",
+    3, "skin_color: white,blue",
+    4, "skin_color: white",
+    5, "skin_color: light",
+    6, "skin_color: white,red",
+    7, "skin_color: unknown",
+    8, "skin_color: green-tan, brown",
+    9, "skin_color: pale",
+    11, "skin_color: dark",
+    12, "skin_color: brown mottle",
+    13, "skin_color: grey",
+    14, "skin_color: mottled green",
+    15, "skin_color: orange",
+    16, "skin_color: blue,grey",
+    17, "skin_color: grey,red",
+    18, "skin_color: white,blue",
+    19, "skin_color: grey, green, yellow"
+    ];
+console.log(arreglotiposkin);
+// ## 4) Busque los tipos de "hair_color" en el arreglo `people.json`
+var arreglotiposhair= [];
+arreglotiposhair =[
+    1, "hair_color: blond",
+    2, "hair_color: n/a",
+    3, "hair_color: none",
+    4, "hair_color: brown",
+    5, "hair_color: brown, grey",
+    6, "hair_color: black",
+    7, "hair_color: auburn, white",
+    8, "hair_color: auburn, grey",
+    9, "hair_color: white",
+    11, "hair_color: grey",
+    12, "hair_color: brown mottle",
+    13, "hair_color: grey",
+   ];
+console.log(arreglotiposhair);
+//## 5) Clasifique y cree diferentes arreglos dependiendo del gender, eye_color, skin y hair_color.
